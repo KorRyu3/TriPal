e3f972aa4ce3421a15a61e493612e7b7f95e48d3
KorRyu3 <146335193+KorRyu3@users.noreply.github.com>
2023-11-04 18:57:43 +0900

    [update] LCELでFunction callingを呼び出せるように調整
    LangChainのAgentsとTool機能を使い、モデルが適宜関数を呼び出せるようにしました。
    また、LCELでモデル・プロンプト・会話履歴・Function callingを結びつけました。
    それに伴って、履歴の保存方法をと関数の引数名を少し変更しました。
    ※ 一番上のsquare関数は仮で定義しているだけです。

--- Web-site/backend/tripalgpt.py
+++ Web-site/backend/tripalgpt.py
@@ -3,10 +3,26 @@ import os
 from operator import itemgetter
 from dotenv import load_dotenv
 
+# LangChainのモジュールをインポート
 from langchain.chat_models import AzureChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.schema.runnable import RunnablePassthrough, RunnableLambda
 from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.tools.render import format_tool_to_openai_function
+from langchain.agents.format_scratchpad import format_to_openai_functions
+from langchain.agents.output_parsers import OpenAIFunctionsAgentOutputParser
+from langchain.agents import Tool
+from langchain.agents import AgentExecutor
+
+
+# Function Callingで利用する関数
+# 入力の二乗を返す関数
+def square(x):
+    x = int(x)
+    return x ** 2
+
+
+
 
 
 class TriPalGPT:
@@ -36,49 +52,79 @@ class TriPalGPT:
             あなたは、ユーザーの質問に答えるAIアシスタントです。
         """
         self.prompt = ChatPromptTemplate.from_messages([
+            # system pronptの定義
             ("system", system_prompt),
+            # 履歴を取得
             MessagesPlaceholder(variable_name="history"),
-            ("human", "{input}")
+            # userの入力
+            ("human", "{input}"),
+            # agent機能
+            MessagesPlaceholder(variable_name="agent_scratchpad"),
         ])
 
         # メモリーの初期化
         self.memory = ConversationBufferMemory(memory_key="history", return_messages=True)
 
-    # Chainの作成
-    def create_chain(self):
-            
+        # function callingで利用するツールの初期化
+        self.tools = [
+            Tool(
+                name="Computing_Squares",
+                func=square,
+                description="二乗の計算をする際に使います。入力は整数のみです。e.g. 2",
+            ),
+        ]
+
+    
+    def cre_agent_exe(self):
+        
         # LangChainのLCELを利用して、Chainを作成する
         history = self.memory.load_memory_variables
+
+        # Toolで定義した関数を、Function callingで利用できるように変換する
+        model_with_tools = self.model.bind(functions=[format_tool_to_openai_function(t) for t in self.tools])
+
+        agent = {
+            
+            "input": lambda x: x["input"],
+            "agent_scratchpad": lambda x: format_to_openai_functions(
+                x["intermediate_steps"]
+            ),
         # itemgetter("history") は、memory.load_memory_variablesの戻り値の中から、historyの値を取り出す
         # 詳細は"https://python.langchain.com/docs/expression_language/cookbook/memory"を参照
-        chain = RunnablePassthrough.assign(
+        } | RunnablePassthrough.assign(
             history=RunnableLambda(history) | itemgetter("history")
-        ) | self.prompt | self.model
+        ) | self.prompt | model_with_tools | OpenAIFunctionsAgentOutputParser()
 
-        return chain
+        # agent_executor = AgentExecutor(agent=agent, tools=tools, memory=memory, verbose=True)
+        agent_executor = AgentExecutor(agent=agent, tools=self.tools, verbose=True)
+
+
+        return agent_executor
+
 
 
     # 履歴を保存する
-    def memory_response(self, user_res):
+    def _memory_response(self, input):
 
         # Chainの作成
-        chain = self.create_chain()
+        chain = self.cre_agent_exe()
 
         # ユーザーからの入力を取得する
-        inputs = {"input": user_res}
+        input = {"input": input}
 
         # 履歴を元に、Chainを実行する
-        res = chain.invoke(input=inputs)
+        res = chain.invoke(input=input)
+        output = res["output"]
         # 履歴を保存する
-        self.memory.save_context(inputs, {"output": res.content})
+        self.memory.save_context(input, {"output": output})
 
-        return res.content
+        return output
 
 
     # ユーザーからの入力を取得する
-    def get_response(self, user_inp):
+    def get_response(self, input):
         # memory_responseメソッドを呼び出して、応答を取得する
-        ai_res = self.memory_response(user_res=user_inp)
+        output = self._memory_response(input=input)
 
-        return ai_res
+        return output
 
