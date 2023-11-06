46c1b5b58887615343439288d5e5f722c649e1c2
KorRyu3 <146335193+KorRyu3@users.noreply.github.com>
2023-11-06 14:23:46 +0900

    [add] LLMを調教して、出力形式をHTMLにさせました。
    Azure
    OpenAIを二回呼び出して実現しています。プロンプトは英語にしています。
    
    呼び出し一度目は質問に対して回答してもらう。
    二度目は、一度目の出力をHTMLにして出力してもらう。
    
    プロンプトはまだまだこれからなので、かなり実験段階です。

--- Web-site/backend/tripalgpt.py
+++ Web-site/backend/tripalgpt.py
@@ -1,7 +1,10 @@
+from functool import *
+
 import openai
 import os
-from operator import itemgetter
 from dotenv import load_dotenv
+
+from operator import itemgetter
 
 # LangChainのモジュールをインポート
 from langchain.chat_models import AzureChatOpenAI
@@ -23,6 +26,15 @@ def square(x):
 
 
 
+tool = [
+    Tool(
+        name='my_tool', 
+        func=my_tool_func,
+        description='ツールの説明',
+        args_schema=MyToolInputSchema
+    ),
+]
+
 
 
 class TriPalGPT:
@@ -43,13 +55,33 @@ class TriPalGPT:
         self.model = AzureChatOpenAI(
             deployment_name = "TriPalGPT",
             model_name="gpt-35-turbo",
-            temperature=0.5,
+            temperature=0.7,
             model_version="0613"
         )
 
         # プロンプトの初期化
+        # system_prompt = """
+        #     # 指示
+        #     あなたは、旅行提案アドバイザーです。
+        #     以下の条件とユーザーの要望に合わせて、旅行の提案を行います。
+        #     例えば、ユーザーが「東京に行きたい」と言った場合、「東京には、〇〇が有名です。なので、おすすめのプランは〜」というように、旅行の提案を行います。
+            
+        #     # 条件
+        #     - {{出発先・到着先・日程・予算・詳細}}の条件のいずれかを入力させるようにしてください。
+        #     - 単体の条件のみが入力された場合、その他も入力させるように会話を促してください。
+        #     - 出力言語は日本語
+        #     - 出力はMarkdown形式
+        # """
         system_prompt = """
-            あなたは、ユーザーの質問に答えるAIアシスタントです。
+            # Instructions
+            You are a travel proposal advisor. Based on the following conditions and user requests, you will provide travel recommendations. 
+            For example, if a user says, "I want to go to Tokyo," you should provide a travel proposal like "Tokyo is famous for ○○, so I recommend the following plan."
+
+            # Conditions
+            - Please allow input for any of the following conditions: {{departure location, destination, travel dates, budget, details}}.
+            - If only one condition is provided, prompt for the remaining conditions in the conversation.
+            - {{Output language is Japanese.}}
+            - {{Output format is Markdown.}}
         """
         self.prompt = ChatPromptTemplate.from_messages([
             # system pronptの定義
@@ -75,7 +107,7 @@ class TriPalGPT:
         ]
 
     
-    def cre_agent_exe(self):
+    def _cre_agent_exe(self):
         
         # LangChainのLCELを利用して、Chainを作成する
         history = self.memory.load_memory_variables
@@ -101,30 +133,99 @@ class TriPalGPT:
 
         return agent_executor
 
+    def html_cre(self, input_):
+        # system_prompt = """
+        # あなたはWebサイトの開発者です。
+        # 入力されたテキストを、適切なHTML形式に変換してください。
+        # 例えば、ユーザーが「こんにちは」と入力した場合、「<div>こんにちは</div>」というように、HTML形式に変換してください。
+        # 入力された文章は{{絶対に変更しない}}でください。
+        # {{出力言語は日本語}}です。
+        # もちろん{{出力はHTMLのみ}}です。
+        # """
+        system_prompt = """
+        You are a professional web developer. 
+        Please convert the entered text into the appropriate HTML format. 
+        For example, if a user enters "こんにちは" (Hello), convert it into HTML format as "<div>こんにちは</div>". 
+        Also, ensure that you do {{not modify the input text}}. 
+        {{The output language is Japanese}}, and, of course, {{the output is HTML only}}.
+        If needed, adjust the design using the style attribute in tags. For example, {{for elements like table tags, you can modify attributes such as borders}}.
+        """
+        html_prompt = ChatPromptTemplate.from_messages([
+            # system pronptの定義
+            ("system", system_prompt),
+            # 例
+            ("human", "こんにちは！今日の予定を考えてみました！[1日目]・仕事をする・お風呂に入る・ご飯を食べる・寝る [2日目]・休憩を取る・ご飯を食べる・寝る[3日目]・寝る"),
+            ("ai", 
+                """<div>
+                        こんにちは！今日の予定を考えてみました！<br>
+                        <br>
+                        [1日目]<br>
+                        <ul>
+                            <li>仕事をする</li>
+                            <li>お風呂に入る</li>
+                            <li>ご飯を食べる</li>
+                            <li>寝る</li>
+                        </ul>
+                        <br>
+                        [2日目]<br>
+                        <ul>
+                            <li>休憩を取る</li>
+                            <li>ご飯を食べる</li>
+                            <li>寝る</li>
+                        </ul>
+                        <br>
+                        [3日目]<br>
+                        <ul>
+                            <li>寝る</li>
+                        </ul>
+                    </div>"""
+            ),
+            
+            # userの入力
+            ("human", "{input}"),
+        ])
+        # Chainの作成
+        html_chain = html_prompt | self.model
+
+        # ユーザーからの入力を取得する
+        input_ = {"input": input_}
+
+        # 履歴を元に、Chainを実行する
+        res = html_chain.invoke(input=input_)
+        print(res)
+        output = res.content
+
+        return output
+
 
 
     # 履歴を保存する
-    def _memory_response(self, input):
+    def _memory_response(self, input_):
 
         # Chainの作成
-        chain = self.cre_agent_exe()
+        chain = self._cre_agent_exe()
 
         # ユーザーからの入力を取得する
-        input = {"input": input}
+        input_ = {"input": input_}
 
         # 履歴を元に、Chainを実行する
-        res = chain.invoke(input=input)
+        res = chain.invoke(input=input_)
         output = res["output"]
         # 履歴を保存する
-        self.memory.save_context(input, {"output": output})
+        self.memory.save_context(input_, {"output": output})
+
+        print(output)
+
+        # 返答をHTML形式に変換する
+        output = self.html_cre(output)
 
         return output
 
 
     # ユーザーからの入力を取得する
-    def get_response(self, input):
+    def get_response(self, input_):
         # memory_responseメソッドを呼び出して、応答を取得する
-        output = self._memory_response(input=input)
+        output = self._memory_response(input_=input_)
 
         return output
 
