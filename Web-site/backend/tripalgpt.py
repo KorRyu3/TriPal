883bc899e458bd5cedf688177d4d1bbe70ec5bc0
KorRyu3 <146335193+KorRyu3@users.noreply.github.com>
2023-11-08 19:54:43 +0900

    [update] プロンプトとクラスの初期化処理の調整
    プロンプトエンジニアリングの調整をしました。
    また、これらのプロンプトを作成する処理を、クラスの初期化時に実行するようにしました。
    
    外部で使わないself付きの関数/変数は、関数/変数の前に「 _ 」をつけて、擬似的なPrivate属性を表現するようにしました。

--- Web-site/backend/tripalgpt.py
+++ Web-site/backend/tripalgpt.py
@@ -1,4 +1,4 @@
-from functool import *
+from func_call_tools import *
 
 import openai
 import os
@@ -52,38 +52,46 @@ class TriPalGPT:
 
 
         # チャットモデルの初期化
-        self.model = AzureChatOpenAI(
+        self._model = AzureChatOpenAI(
             deployment_name = "TriPalGPT",
             model_name="gpt-35-turbo",
             temperature=0.7,
             model_version="0613"
         )
 
+        
         # プロンプトの初期化
+        system_prompt = """
+            # Instructions
+            You are a travel consultant.
+            Based on the following conditions and user requests, you will provide travel recommendations. 
+            For example, if a user says, "I want to go to Tokyo," you should provide a travel proposal like "Tokyo is famous for ○○, so I recommend the following plan."
+
+            # Conditions
+            - Create a detailed travel schedule by having the user enter one of the following criteria: {{departure}}, {{destination}}, {{dates (length of trip)}}, {{budget}}, and {{detail information}}.
+            - If only one condition is provided, prompt for the remaining conditions in the conversation.
+            - The schedule should include recommended activities, recommended accommodations, transportation options, and meal plans.
+            - Tips for navigating local culture, customs, and necessary travel notes should also be generated.
+            - If there is information that you do not know or do not know, {{please answer honestly, "I don't know."}} Or, use function calling to answer the question.
+            - {{Output language is Japanese}}.
+            - {{Output format is Markdown}}.
+        """
         # system_prompt = """
         #     # 指示
-        #     あなたは、旅行提案アドバイザーです。
+        #     あなたは旅行コンサルタントです。
         #     以下の条件とユーザーの要望に合わせて、旅行の提案を行います。
         #     例えば、ユーザーが「東京に行きたい」と言った場合、「東京には、〇〇が有名です。なので、おすすめのプランは〜」というように、旅行の提案を行います。
             
         #     # 条件
-        #     - {{出発先・到着先・日程・予算・詳細}}の条件のいずれかを入力させるようにしてください。
-        #     - 単体の条件のみが入力された場合、その他も入力させるように会話を促してください。
+        #     - {{出発先}}と{{目的地}}、{{日程(旅行期間)}}、{{予算}}、{{詳細情報}}の条件のいずれかを入力させ、詳細な旅行予定を作成してください。
+        #     - 単体の条件のみが入力された場合、その他も入力させるように会話を続けなさい
+        #     - 予定には、おすすめのアクティビティ、おすすめの宿泊施設、交通手段のオプション、食事予定などを含める必要があります。
+        #     - 現地の文化、習慣をナビゲートするためのヒント、および必要な旅行上の注意事項も生成してください。
+        #     - {{わからない、知らない情報があれば、素直に「わかりません」と答えてください。}}もしくは、function callingを活用し、答えてください。
         #     - 出力言語は日本語
         #     - 出力はMarkdown形式
         # """
-        system_prompt = """
-            # Instructions
-            You are a travel proposal advisor. Based on the following conditions and user requests, you will provide travel recommendations. 
-            For example, if a user says, "I want to go to Tokyo," you should provide a travel proposal like "Tokyo is famous for ○○, so I recommend the following plan."
-
-            # Conditions
-            - Please allow input for any of the following conditions: {{departure location, destination, travel dates, budget, details}}.
-            - If only one condition is provided, prompt for the remaining conditions in the conversation.
-            - {{Output language is Japanese.}}
-            - {{Output format is Markdown.}}
-        """
-        self.prompt = ChatPromptTemplate.from_messages([
+        self._prompt = ChatPromptTemplate.from_messages([
             # system pronptの定義
             ("system", system_prompt),
             # 履歴を取得
@@ -94,69 +102,32 @@ class TriPalGPT:
             MessagesPlaceholder(variable_name="agent_scratchpad"),
         ])
 
-        # メモリーの初期化
-        self.memory = ConversationBufferMemory(memory_key="history", return_messages=True)
 
-        # function callingで利用するツールの初期化
-        self.tools = [
-            Tool(
-                name="Computing_Squares",
-                func=square,
-                description="二乗の計算をする際に使います。入力は整数のみです。e.g. 2",
-            ),
-        ]
-
-    
-    def _cre_agent_exe(self):
-        
-        # LangChainのLCELを利用して、Chainを作成する
-        history = self.memory.load_memory_variables
-
-        # Toolで定義した関数を、Function callingで利用できるように変換する
-        model_with_tools = self.model.bind(functions=[format_tool_to_openai_function(t) for t in self.tools])
-
-        agent = {
-            
-            "input": lambda x: x["input"],
-            "agent_scratchpad": lambda x: format_to_openai_functions(
-                x["intermediate_steps"]
-            ),
-        # itemgetter("history") は、memory.load_memory_variablesの戻り値の中から、historyの値を取り出す
-        # 詳細は"https://python.langchain.com/docs/expression_language/cookbook/memory"を参照
-        } | RunnablePassthrough.assign(
-            history=RunnableLambda(history) | itemgetter("history")
-        ) | self.prompt | model_with_tools | OpenAIFunctionsAgentOutputParser()
-
-        # agent_executor = AgentExecutor(agent=agent, tools=tools, memory=memory, verbose=True)
-        agent_executor = AgentExecutor(agent=agent, tools=self.tools, verbose=True)
-
-
-        return agent_executor
-
-    def html_cre(self, input_):
-        # system_prompt = """
+        # HTMLのプロンプトの初期化
+        html_system_prompt = """
+            You are a professional web developer. 
+            Please convert the entered text into the appropriate {{HTML format}}. 
+            For example, if a user enters "こんにちは" (Hello), convert it into HTML format as "<div>こんにちは</div>". 
+            Also, ensure that you {{do not modify the input text}}. 
+            {{The output language is Japanese}}, and, of course, {{the output is HTML only}}.
+            {{Always start and end with a div tag.}}
+            If needed, {{adjust the design using the style attribute in tags}}. For example, for elements like {{table tags}}, you can {{modify attributes such as borders}}.
+        """
         # あなたはWebサイトの開発者です。
-        # 入力されたテキストを、適切なHTML形式に変換してください。
+        # 入力されたテキストを、適切な{{HTML形式}}に変換してください。
         # 例えば、ユーザーが「こんにちは」と入力した場合、「<div>こんにちは</div>」というように、HTML形式に変換してください。
         # 入力された文章は{{絶対に変更しない}}でください。
         # {{出力言語は日本語}}です。
         # もちろん{{出力はHTMLのみ}}です。
+        # {必ずdivタグで始まり、divタグで終わる}}。
+        # 必要に応じて、タグのstyle属性を使ってデザインを調整します。例えば、{{tableタグのような要素では、bordersなどの属性を変更することができます}}。
         # """
-        system_prompt = """
-        You are a professional web developer. 
-        Please convert the entered text into the appropriate HTML format. 
-        For example, if a user enters "こんにちは" (Hello), convert it into HTML format as "<div>こんにちは</div>". 
-        Also, ensure that you do {{not modify the input text}}. 
-        {{The output language is Japanese}}, and, of course, {{the output is HTML only}}.
-        If needed, adjust the design using the style attribute in tags. For example, {{for elements like table tags, you can modify attributes such as borders}}.
-        """
-        html_prompt = ChatPromptTemplate.from_messages([
+        self._html_prompt = ChatPromptTemplate.from_messages([
             # system pronptの定義
-            ("system", system_prompt),
+            ("system", html_system_prompt),
             # 例
             ("human", "こんにちは！今日の予定を考えてみました！[1日目]・仕事をする・お風呂に入る・ご飯を食べる・寝る [2日目]・休憩を取る・ご飯を食べる・寝る[3日目]・寝る"),
-            ("ai", 
-                """<div>
+            ("ai", """<div>
                         こんにちは！今日の予定を考えてみました！<br>
                         <br>
                         [1日目]<br>
@@ -184,15 +155,55 @@ class TriPalGPT:
             # userの入力
             ("human", "{input}"),
         ])
-        # Chainの作成
-        html_chain = html_prompt | self.model
 
-        # ユーザーからの入力を取得する
-        input_ = {"input": input_}
+        # メモリーの初期化
+        self.memory = ConversationBufferMemory(memory_key="history", return_messages=True)
 
-        # 履歴を元に、Chainを実行する
-        res = html_chain.invoke(input=input_)
+        # function callingで利用するツールの初期化
+        self.tools = [
+            Tool(
+                name="Computing_Squares",
+                func=square,
+                description="二乗の計算をする際に使います。入力は整数のみです。e.g. 2",
+            ),
+        ]
+
+    
+    def _cre_agent_exe(self):
+        
+        # LangChainのLCELを利用して、Chainを作成する
+        history = self.memory.load_memory_variables
+
+        # Toolで定義した関数を、Function callingで利用できるように変換する
+        model_with_tools = self._model.bind(functions=[format_tool_to_openai_function(t) for t in self.tools])
+
+        agent = {
+            
+            "input": lambda x: x["input"],
+            "agent_scratchpad": lambda x: format_to_openai_functions(
+                x["intermediate_steps"]
+            ),
+        # itemgetter("history") は、memory.load_memory_variablesの戻り値の中から、historyの値を取り出す
+        # 詳細は"https://python.langchain.com/docs/expression_language/cookbook/memory"を参照
+        } | RunnablePassthrough.assign(
+            history=RunnableLambda(history) | itemgetter("history")
+        ) | self._prompt | model_with_tools | OpenAIFunctionsAgentOutputParser()
+
+        # agent_executor = AgentExecutor(agent=agent, tools=tools, memory=memory, verbose=True)
+        agent_executor = AgentExecutor(agent=agent, tools=self.tools, verbose=True)
+
+
+        return agent_executor
+
+    def _html_cre(self, user_input):
+
+        # HTMLのChainの作成
+        html_chain = self._html_prompt | self._model
+
+        # TriPalGPTからの出力を元に、Chainを実行する
+        res = html_chain.invoke(input={"input": user_input})
         print(res)
+
         output = res.content
 
         return output
@@ -200,32 +211,33 @@ class TriPalGPT:
 
 
     # 履歴を保存する
-    def _memory_response(self, input_):
+    def _memory_response(self, user_input):
 
         # Chainの作成
         chain = self._cre_agent_exe()
 
         # ユーザーからの入力を取得する
-        input_ = {"input": input_}
+        user_input = {"input": user_input}
 
         # 履歴を元に、Chainを実行する
-        res = chain.invoke(input=input_)
+        res = chain.invoke(input=user_input)
         output = res["output"]
         # 履歴を保存する
-        self.memory.save_context(input_, {"output": output})
+        self.memory.save_context(user_input, {"output": output})
 
         print(output)
 
-        # 返答をHTML形式に変換する
-        output = self.html_cre(output)
 
         return output
 
 
     # ユーザーからの入力を取得する
-    def get_response(self, input_):
+    def get_response(self, user_input):
         # memory_responseメソッドを呼び出して、応答を取得する
-        output = self._memory_response(input_=input_)
+        output = self._memory_response(user_input=user_input)
+
+        # 返答をHTML形式に変換する
+        output = self._html_cre(output)
 
         return output
 
