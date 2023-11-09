b05cf17768aadddc48c00be408489c4bdc7d7779
KorRyu3 <146335193+KorRyu3@users.noreply.github.com>
2023-11-09 16:48:51 +0900

    [update] API配布の前準備
    APIが配布されたらすぐに起動できるように前準備をしました。
    恐らくこれに順守した挙動になるはず

--- Web-site/backend/func_call_tools.py
+++ Web-site/backend/func_call_tools.py
@@ -11,27 +11,32 @@ from pydantic.v1 import BaseModel , Field
 # https://docs.pydantic.dev/1.10/usage/schema/
 
 import requests
+import os
+from dotenv import load_dotenv
+import json
 
+# 環境変数をロード
+load_dotenv()
 
 # Toolで利用する関数の入力スキーマの定義例
-class MyToolInputSchema(BaseModel):
+class TravelProposalSchema(BaseModel):
 
-    text: str = Field(
+    loc_name: str = Field(
         # デフォルト値を設定することができる
-        default='default',
+        default=None,
         # このフィールドのタイトル
-        title='Password',
+        title='LocationName',
         # 詳細な説明を記載することができる
-        description='Password of the user',
+        description='Text to use for searching based on the name of the location. Required parameter.',
         # e.g.の記載
-        examples= ['123456'],
+        examples= ['日本', 'America', '東京都', '大宮市', '東京タワー', '旭山動物園', 'サイゼリヤ', '別府温泉杉乃井ホテル'],
     )
 
-    count: int = Field(
-        default=1,
-        title='Count',
-        description='カウントする数',
-        examples=['12', '14'],
+    category: str = Field(
+        default="",
+        title='Category',
+        description='Filters result set based on property type. Valid options are "hotels", "attractions", "restaurants", and "geos". Arbitrary parameter',
+        examples=['hotels', 'attractions', 'restaurants', 'geos'],
     )
 
 
@@ -39,31 +44,54 @@ class MyToolInputSchema(BaseModel):
 # print(MyToolInputSchema.schema_json(indent=3))
 
 
-# Toolで利用する関数の定義例
-def my_tool_func(**kwargs):
-    # 辞書の引数を取り出す
-    text = kwargs.get("text", "default")
-    
-    count = kwargs["count"]
-    
-    # ツールの処理
-
-    output = "output"
-    return output
-
-
-
+# Tool(Function Calling)で利用する関数の定義
 
 # 観光スポットの提案
-def search_hotel_vacancy(**kwargs):
-    param = ""
+def suggested_sightseeing_spots(**kwargs: dict) -> str:
+    key: str = os.environ.get("TRIADO_API_KEY")
+    headers = {"accept": "application/json"}
 
-    place_id = kwargs.get("place_id")
-    
+    # ツールの処理
+    # 辞書の引数を取り出す
+    loc_name = kwargs.get("loc_name")
+    category = kwargs.get("category")
+    language = "ja"
 
-    res_json = requests.get("URL")
+    print(loc_name, category)
+
+
+    # 場所のidを取得
+    id_param = "?key=" + key
+
+    if loc_name is None:
+        return "検索したい場所の名前を入力してください"
+    else:
+        id_param += "&searchQuery=" + loc_name
+
+    if category is not "":
+        id_param += "&category=" + category
+
+    id_param += "&language=" + language
+    url = "https://api.content.tripadvisor.com/api/v1/location/search"
+    print(url + id_param)
+    response = requests.get(url + id_param, headers=headers)
+    print(response)
+
+    # jsonの中身を取り出す
+    # response = json.loads(response)
+    res_loc_id = response.text
+
+
+    # 場所の情報を取得
+    loc_id = res_loc_id
+    language = "ja"
+    currency = "JPY"
+
+    loc_param = f"/{loc_id}/details?key={key}&language={language}&currency={currency}"
+    url = "https://api.content.tripadvisor.com/api/v1/location"
+    response = requests.get(url + loc_param, headers=headers)
     
     # jsonの中身を取り出す
+    output = response.text
 
-
-    return "output"
+    return output
