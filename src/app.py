f0536164c6a8100578198d852803b198bb27db1a
KorRyu3 <146335193+KorRyu3@users.noreply.github.com>
2024-01-06 21:47:59 +0900

    [update] 通信方法をWebSocketに変更(サーバー側)
    送受信を一つのrouteで行いたいため、WebSocketを使用するように変更しました。
    なんかすっごいコードがシンプルになった気がする。
    
    ただ、WebSocketの通信は、ユーザーが増加するとサーバーの負荷が増えるので、将来的には他の方法に変更するかもしれません。
    正直、WebSocketの使い方がよくわかってないので、もっと勉強してからにしたいです。

--- src/app.py
+++ src/app.py
@@ -1,11 +1,7 @@
-from typing import AsyncGenerator
-import json
 import asyncio
 
-from pydantic import BaseModel
 # FastAPI
-from fastapi import FastAPI, Request
-from fastapi.responses import StreamingResponse
+from fastapi import FastAPI, Request, WebSocket
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 import uvicorn
@@ -17,16 +13,15 @@ app = FastAPI()
 # 原因は本当に不明
 # おそらく、こいつが参照するdirectoryが、appから見たものではなく、作業directoryから見たものだと推測できる
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
 # template engineの設定
 templates = Jinja2Templates(directory="templates")
+
 # LLMの初期化
 tripal_gpt = TriPalGPT()
 
 
-# ユーザーの入力を受け取るためのスキーマ
-class UserInput(BaseModel):
-    user_chat: str
-
+# HTMLをレンダリングするだけの関数
 @app.get('/')
 def index(request: Request):
     return templates.TemplateResponse(
@@ -34,26 +29,32 @@ def index(request: Request):
         name='index.html',
     )
 
-@app.post('/chat')
-async def chat(body: UserInput):
-    print("body: ", body)
-    print("body(type): ", type(body))
-    print("body.user_chat: ", body.user_chat)
+# Websocketを使用して、一つのrouteで送受信ができるようにする
+# そうしないと、入力と出力が一緒にできず、他の人が入力した内容で出力してしまう可能性がある
+# sessionがうんちゃらかんちゃらってhiroさんが言ってた。
+# sessionって何ですか？？？？？？？？？？？？？？？？？？？？
+@app.websocket('/chat')
+async def chat(ws: WebSocket):
+    # Websocketの接続を確立
+    await ws.accept()
 
-    # チャットボットにユーザーの入力を渡して、応答を取得する
-    # responseは非同期generator
-    async def async_generator_output() -> AsyncGenerator[str, None]:
-        async for output in tripal_gpt.get_async_generator_output(user_input=body.user_chat):
-            print("output: ", output)
-            data = f"data: {json.dumps({'message': output})}"
-            yield data + "\n\n"
+    # Websocketの接続が切れるまで、ユーザーの入力を受け取る
+    while True:
+        # ユーザーの入力を受け取る
+        user_chat = await ws.receive_text()
+        print("user_chat: ", user_chat)
+
+        # チャットボットにユーザーの入力を渡して、応答を取得する
+        # responseは非同期generator
+        async for output in tripal_gpt.get_async_generator_output(user_input=user_chat):
+            # print("output: ", output)
+            # 応答を送信する
+            # UXのために、0.05秒待つ
+            # 0秒だと早すぎて目で追えない
+            await asyncio.sleep(0.05)
+            # Websocketに応答を送信
+            await ws.send_text(output)
             await asyncio.sleep(0)
-
-    # Responseオブジェクトを作成する
-    return StreamingResponse(
-            content=async_generator_output(),
-            media_type='text/event-stream',
-        )
 
 
 if __name__ == '__main__':
