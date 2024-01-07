8456c4648973a5f6dbbd87756aa9cc92d2ce0296
KorRyu3 <146335193+KorRyu3@users.noreply.github.com>
2024-01-07 11:19:59 +0900

    [update] LangChainのdebug文の削除

--- src/app.py
+++ src/app.py
@@ -56,4 +56,5 @@ async def chat(ws: WebSocket):
 
 
 if __name__ == '__main__':
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    # uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000)
