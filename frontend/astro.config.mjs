8fb0387a1020dbac066d5df5900a64a5ca9f69e4
nka21 <133028205+nka21@users.noreply.github.com>
2024-02-02 02:23:41 +0900

    tailwindcssのインストール

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   outDir: "../backend/static",
   base: "/backend/build/",
+  integrations: [tailwind()]
 });
